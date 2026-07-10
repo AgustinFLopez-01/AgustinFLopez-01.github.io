fd553c6f602cc216a0a1bdda952850c38b3047bc
Agustin Flores Lopez <afl231001@gmail.com>
2026-07-10 17:11:43 -0600

    comportamiento de busqueda

--- js/Mexcar.js
+++ js/Mexcar.js
@@ -1,18 +1,69 @@
 const autos = {
-    "Marca 1": {
-        "Modelo A": [2020, 2021, 2022],
+    "Nissan": {
+        "Versa": [2022, 2023],
         "Modelo B": [2018, 2019]
     },
-    "Marca 2": {
-        "Modelo C": [2015, 2016],
+    "Toyota": {
+        "Corolla": [2021,2022],
         "Modelo D": [2023]
     },
-    "Marca 3": {
-        "Modelo E": [2010, 2011, 2012],
+    "Kia": {
+        "Seltos": [2026],
         "Modelo F": [2018, 2020]
     }
 };
-
+const inventario = [
+    {
+        marca: "Nissan",
+        modelo: "Versa",
+        anio: 2022,
+        color: "Rojo",
+        lugares: [
+            "A3",
+            "A4",
+            "B2"
+        ]
+    },
+    {
+        marca: "Nissan",
+        modelo: "Versa",
+        anio: 2023,
+        color: "Azul",
+        lugares: [
+            "B1"
+        ]
+    },
+    {
+        marca: "Toyota",
+        modelo: "Corolla",
+        anio: 2021,
+        color: "Blanco",
+        lugares: [
+            "B3",
+            "A2",
+            "A1"
+        ]
+    },
+    {
+        marca: "Toyota",
+        modelo: "Corolla",
+        anio: 2022,
+        color: "Negro",
+        lugares: [
+            "C3"
+        ]
+    },
+    {
+        marca: "Kia",
+        modelo: "Seltos",
+        anio: 2026,
+        color: "Rojo",
+        lugares: [
+            "C2",
+            "C1"
+        ]
+    }
+];
 const marca = document.getElementById("Marca");
 const modelo = document.getElementById("Modelo");
 const anio = document.getElementById("Anio");
@@ -46,3 +97,41 @@ modelo.addEventListener("change", function () {
         anio.appendChild(opcion);
     }
 });
+function buscarAutos() {
+
+    const marca = document.getElementById("Marca").value;
+    const modelo = document.getElementById("Modelo").value;
+    const anio = document.getElementById("Anio").value;
+
+    const resultados = document.getElementById("Resultados");
+
+    resultados.innerHTML = "";
+
+    resultados.style.visibility = "visible";
+
+    inventario.forEach(auto => {
+
+        if (
+            auto.marca === marca &&
+            auto.modelo === modelo &&
+            auto.anio == anio
+        ) {
+
+            auto.lugares.forEach(lugar => {
+
+                const li = document.createElement("li");
+                const boton = document.createElement("button");
+
+                boton.textContent =
+                    `${lugar} | ${auto.marca} ${auto.modelo} ${auto.anio} | ${auto.color}`;
+
+                li.appendChild(boton);
+                resultados.appendChild(li);
+
+            });
+
+        }
+
+    });
+
+}
